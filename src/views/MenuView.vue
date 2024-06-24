58b4185093eb80a7fe3752373f8a4ccfe6584397
tkimanh <tran.kimanh@orange.fr>
2024-06-24 12:52:22 +0200

    [feature/GOL-4] add the button to add a type of vessel

---
+++ src/views/MenuView.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { ref } from 'vue'
+import menuItems from './../js/MenuItems.js'
+import MenuItem from './MenuItemView.vue'
+
+let collapsed = ref(false)
+</script>
+
+<template>
+  <aside :class="['menu', collapsed ? 'collapsed' : 'expanded']">
+    <div class="header">
+      <button class="menu-button" @click.prevent="collapsed = !collapsed">
+        <img
+          width="30"
+          height="30"
+          src="https://img.icons8.com/ios-glyphs/30/menu--v3.png"
+          alt="Bouton menu"
+        />
+      </button>
+    </div>
+
+    <div class="menu__item">
+      <ul>
+        <MenuItem
+          v-for="item in menuItems.items"
+          :key="item.id"
+          :item="item"
+          :collapsed="collapsed"
+        />
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<style scoped>
+.menu {
+  background-color: #ff5555;
+  height: 100vh;
+  transition: width 0.3s;
+  left: 0;
+}
+
+.collapsed {
+  width: 5rem;
+  /* height: 4.4rem; */
+}
+
+.expanded {
+  width: 15rem;
+  background-color: #ff5555;
+}
+
+.header {
+  margin: 0 auto;
+  display: flex;
+  justify-content: end;
+}
+
+.menu-button {
+  width: 4.4rem;
+  height: 4.4rem;
+  border: none;
+  background-color: transparent;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.profile {
+  display: flex;
+  padding: var(--separacion);
+  gap: var(--separacion);
+  align-items: center;
+}
+
+.profile__img {
+  width: 4.4rem;
+}
+
+.profile__name {
+  text-align: center;
+}
+
+.profile__name a {
+  text-decoration: none;
+  text-transform: capitalize;
+}
+
+.menu__item ul {
+  padding: 0;
+  list-style: none;
+  margin: 0;
+}
+</style>
